71cac6349d5d5595a9dd754f0719930323641d44
Darth Varg <cbvargas123@gmail.com>
2020-03-12 16:26:01 -0700

    Finished Schemas

---
+++ database/schemas.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const db = require('./index.js');
+const Schema = mongoose.Schema;
+
+module.exports = {
+  listing: new Schema({
+    name: String, // 
+    type: String, //stay, experience etc...
+    state: String,
+    country: String,
+    city: String,
+    images: [{ image: String }], //imageurls
+  }),
+
+  languages: new Schema ({
+    name: String,
+    country: String
+  }),
+
+  currency: new Schema ({
+    name: String,
+    symbol: String
+  })
+};
